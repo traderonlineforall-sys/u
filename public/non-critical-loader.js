69b981baf54da662c9f66bc80aa2e05aaaafaf5e
traderonlineforall-sys <traderonlineforall@gmail.com>
2026-04-11 17:16:58 +0200

    10

---
+++ public/non-critical-loader.js
@@ -0,0 +1,28 @@
+(function(){
+  function loadExtras(){
+    if (loadExtras._done) return;
+    loadExtras._done = true;
+    var mods = [
+      './online-users-count.js',
+      './eid-hud.js',
+      './layout-stabilizer.js'
+    ];
+    mods.forEach(function(src){
+      import(src).catch(function(){});
+    });
+  }
+
+  function schedule(){
+    if ('requestIdleCallback' in window) {
+      window.requestIdleCallback(loadExtras, { timeout: 1800 });
+    } else {
+      setTimeout(loadExtras, 900);
+    }
+  }
+
+  if (document.readyState === 'complete') {
+    schedule();
+  } else {
+    window.addEventListener('load', schedule, { once: true });
+  }
+})();
