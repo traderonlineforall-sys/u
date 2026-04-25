5267cdbc70b164684595b5e2e578b5394d9ce571
eslamnett-lang <eslamnett@gmail.com>
2026-04-25 07:47:52 +0300

    998

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,14 @@
 export const metadata = {
-  title: "SR Tool",
+  title: "UWK07",
+  icons: {
+    icon: [
+      { url: "/favicon.ico" },
+      { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+      { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" }
+    ],
+    shortcut: "/favicon.ico",
+    apple: "/apple-touch-icon.png"
+  }
 };
 
 export default function RootLayout({ children }) {
