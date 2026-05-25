a82c65c7afd6643130a8605bd1a9680e6ff5a7f2
traderonlineforall-sys <traderonlineforall@gmail.com>
2026-05-25 07:53:31 +0300

    5

--- app/layout.js
+++ app/layout.js
@@ -1,13 +1,8 @@
 export const metadata = {
   title: "MyTime",
   icons: {
-    icon: [
-      { url: "/favicon.ico" },
-      { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
-      { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" }
-    ],
-    shortcut: "/favicon.ico",
-    apple: "/apple-touch-icon.png"
+    icon: "/favicon.ico",
+    shortcut: "/favicon.ico"
   }
 };
 
